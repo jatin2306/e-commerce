133b442143cc0aea6e292fc66026f2205179ca58
Jatin gupta <jatingupta2306.com>
2024-07-12 00:22:42 +0530

    routefileadded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import HomePage from './Home/HomaPage'
-import Login from './Login/Login'
+import RouteConfig from './RouteConfig'
+
 const App = () => {
   return (
    <>
-<Router>
-  <Routes>
-    <Route path="/" element={<HomePage/>}/>
-    <Route path="/Login" element={<Login/>}/>
-  </Routes>
-</Router>
+<RouteConfig/>
    </>
   )
 }
---
+++ src/RouteConfig.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import HomePage from './Home/HomaPage'
+import Login from './Login/Login'
+
+
+const RouteConfig = () => {
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/Login" element={<Login/>}/>
+    </Routes>
+  </Router>
+  )
+}
+
+export default RouteConfig
